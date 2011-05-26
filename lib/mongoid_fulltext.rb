5f6588ff11ff7619e30e30ad5f2b482b9f7e7977
dblock <dblock@dblock.org>
2011-05-26 14:10:16 -0400

    Keeping max score of ngram in the ngram hash.

--- lib/mongoid_fulltext.rb
+++ lib/mongoid_fulltext.rb
@@ -97,15 +97,19 @@ module Mongoid::FullTextSearch
       end
     end
 
-    def all_ngrams(str, config, bound_number_returned=true)
+    # returns an [ngram, score] [ngram, position] pair
+    def all_ngrams(str, config, bound_number_returned = true)
       return {} if str.nil? or str.length < config[:ngram_width]
       filtered_str = str.downcase.split('').map{ |ch| config[:alphabet][ch] }.find_all{ |ch| !ch.nil? }.join('')
+      
       if bound_number_returned
         step_size = [((filtered_str.length - config[:ngram_width]).to_f / config[:max_ngrams_to_search]).ceil, 1].max
       else
         step_size = 1
       end
-      Hash[(0..filtered_str.length - config[:ngram_width]).step(step_size).map do |i|
+      
+      # array of ngrams that have
+      ngram_ary = (0..filtered_str.length - config[:ngram_width]).step(step_size).map do |i|
         if i == 0 or (config[:apply_prefix_scoring_to_all_words] and \
                       config[:word_separators].has_key?(filtered_str[i-1].chr))
           score = Math.sqrt(1 + 1.0/filtered_str.length)
@@ -113,7 +117,16 @@ module Mongoid::FullTextSearch
           score = Math.sqrt(2.0/filtered_str.length)
         end
         [filtered_str[i..i+config[:ngram_width]-1], score]
-      end]
+      end
+      
+      ngram_hash = {}
+      
+      # deduplicate, and keep the highest score 
+      ngram_ary.each do |ngram, score|
+        ngram_hash[ngram] = [ngram_hash[ngram] || 0, score].max
+      end
+      
+      ngram_hash
     end
 
   end
